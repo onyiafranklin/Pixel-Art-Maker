93f4c2d0e8f696b962f2053984175b3ff671b32c
franklinonyia <bukkyonyia@gmail.com>
2018-06-26 23:08:58 +0100

    arrangement of codes and addition of comments tomakethe code readeable

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Pixel-Art-Maker
-the pixel art maker can be used to draw any shape of your choice
-#Here is the link to this project=>"https://onyiafranklin.github.io/Pixel-Art-Maker/"
+# Title:Pixel-Art-Maker
+#Link to this project=>"https://onyiafranklin.github.io/Pixel-Art-Maker/"
+#description:The pixel art maker can be used to draw any shape of your choice,
+#first the user of the web application inputs the length and width size of his/her grid choice and clicks "submit" button.and the grid appears.
+#Second the user picks the particular color choice using the "color picker".
+#thirdly, with the picked color,the user uses it to click on the selected box on the grid th make an awesome shape of choice.
+#finally the user changes the grid size and clicks on the submit button to create a new grid and the cycle continues.
--- designs.js
+++ designs.js
@@ -2,20 +2,23 @@
 //this variable picks the value of color selected
 let color=$('#colorPicker').val();
 //this variable picks the table element
-const table=$('#pixelCanvas');//
- //this finction forms a grid
+const table=$('#pixelCanvas');
+ //TODO:this function forms a grid
+ /**
+* @description This function forms the grid.
+* @param {number} gridWidthValue - The width of the grid.
+* @param {number} gridHeightValue - The height of the grid.
+*/
 function makeGrid(gridWidthValue,gridHeightValue) {
-	
 		 const size={
-		 gridWidthValue:$('#inputWeight').val(),//width of grid
-		 gridHeightValue:$('#inputHeight').val()//height of gro=id
+					 gridWidthValue:$('#inputWeight').val(),//width of grid
+					 gridHeightValue:$('#inputHeight').val()//height of grid
 		}
-		 	 
-				table.find('tr').remove();//remove any previous table that was there before  a new one will be formed
+				table.find('tr').remove();//remove any previous table that was there before creating a new one will be formed
 			for ( let x=1; x<=size.gridWidthValue;x++){// we form the table row first (tr)
 				let xAxes= '<tr>';
 				let yAxes='';
-				for (let y =1;y<=size.gridHeightValue; y++) {
+					for (let y =1;y<=size.gridHeightValue; y++) {
 					yAxes+='<td></td>';	// we then form the table data
 				}
 				xAxes+=yAxes;//adding of the table row ( opening tag) with the table data
@@ -31,11 +34,12 @@ function makeGrid(gridWidthValue,gridHeightValue) {
  // this function picks the particular selected color and assigns it to a particular td or table data that was clicked
 table.on('click', 'td', function(e){
 	if($(e.target).css('background-color') !== color){//cell color is compared to the color selected from the picker
-	 		//if color cell different from the selected color with color picker
+	 	//if color cell different from the selected color with color picker
  		color = $('#colorPicker').val();
  		$(e.target).css('background-color', color);
 		color = $(e.target).css('background-color'); // get the color value in RGB notation
-	}else{
+	}
+	else{
 		$(e.target).css('background-color', 'inherit');
  	}
  })
